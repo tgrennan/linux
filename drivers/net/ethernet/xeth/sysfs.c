df1e217beb0fc4ecdbbc7850df7eee53a6b0253b
Tom Grennan <tgrennan@platinasystems.com>
2018-03-02 14:14:12 -0800

    xeth: add ethtool stats
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- drivers/net/ethernet/xeth/sysfs.c
+++ drivers/net/ethernet/xeth/sysfs.c
@@ -27,150 +27,172 @@
 
 #include "xeth.h"
 #include "debug.h"
+#include "sysfs.h"
 
-struct xeth_sysfs_netstat {
+struct xeth_sysfs_link_stat {
 	struct	attribute attr;
 	size_t	offset;
 };
 
-#define to_xeth_sysfs_netstat(ap)					\
-	container_of(ap, struct xeth_sysfs_netstat, attr)
-
-#define new_xeth_sysfs_netstat(member)					\
-static struct xeth_sysfs_netstat xeth_sysfs_##member = {		\
-	.attr = {							\
-		.name = #member,					\
-		.mode = S_IRUGO | S_IWUSR,				\
-	},								\
-	.offset = offsetof(struct rtnl_link_stats64, member),		\
-}
-
-new_xeth_sysfs_netstat(rx_packets);
-new_xeth_sysfs_netstat(tx_packets);
-new_xeth_sysfs_netstat(rx_bytes);
-new_xeth_sysfs_netstat(tx_bytes);
-new_xeth_sysfs_netstat(rx_errors);
-new_xeth_sysfs_netstat(tx_errors);
-new_xeth_sysfs_netstat(rx_dropped);
-new_xeth_sysfs_netstat(tx_dropped);
-new_xeth_sysfs_netstat(multicast);
-new_xeth_sysfs_netstat(collisions);
-new_xeth_sysfs_netstat(rx_length_errors);
-new_xeth_sysfs_netstat(rx_over_errors);
-new_xeth_sysfs_netstat(rx_crc_errors);
-new_xeth_sysfs_netstat(rx_frame_errors);
-new_xeth_sysfs_netstat(rx_fifo_errors);
-new_xeth_sysfs_netstat(rx_missed_errors);
-new_xeth_sysfs_netstat(tx_aborted_errors);
-new_xeth_sysfs_netstat(tx_carrier_errors);
-new_xeth_sysfs_netstat(tx_fifo_errors);
-new_xeth_sysfs_netstat(tx_heartbeat_errors);
-new_xeth_sysfs_netstat(tx_window_errors);
-new_xeth_sysfs_netstat(rx_compressed);
-new_xeth_sysfs_netstat(tx_compressed);
-new_xeth_sysfs_netstat(rx_nohandler);
-
-static struct attribute *xeth_sysfs_netstat_attrs[] = {
-	&xeth_sysfs_rx_packets.attr,
-	&xeth_sysfs_tx_packets.attr,
-	&xeth_sysfs_rx_bytes.attr,
-	&xeth_sysfs_tx_bytes.attr,
-	&xeth_sysfs_rx_errors.attr,
-	&xeth_sysfs_tx_errors.attr,
-	&xeth_sysfs_rx_dropped.attr,
-	&xeth_sysfs_tx_dropped.attr,
-	&xeth_sysfs_multicast.attr,
-	&xeth_sysfs_collisions.attr,
-	&xeth_sysfs_rx_length_errors.attr,
-	&xeth_sysfs_rx_over_errors.attr,
-	&xeth_sysfs_rx_crc_errors.attr,
-	&xeth_sysfs_rx_frame_errors.attr,
-	&xeth_sysfs_rx_fifo_errors.attr,
-	&xeth_sysfs_rx_missed_errors.attr,
-	&xeth_sysfs_tx_aborted_errors.attr,
-	&xeth_sysfs_tx_carrier_errors.attr,
-	&xeth_sysfs_tx_fifo_errors.attr,
-	&xeth_sysfs_tx_heartbeat_errors.attr,
-	&xeth_sysfs_tx_window_errors.attr,
-	&xeth_sysfs_rx_compressed.attr,
-	&xeth_sysfs_tx_compressed.attr,
-	&xeth_sysfs_rx_nohandler.attr,
-	NULL,
+static size_t xeth_sysfs_link_stat_offsets[] = {
+	offsetof(struct rtnl_link_stats64, rx_packets),
+	offsetof(struct rtnl_link_stats64, tx_packets),
+	offsetof(struct rtnl_link_stats64, rx_bytes),
+	offsetof(struct rtnl_link_stats64, tx_bytes),
+	offsetof(struct rtnl_link_stats64, rx_errors),
+	offsetof(struct rtnl_link_stats64, tx_errors),
+	offsetof(struct rtnl_link_stats64, rx_dropped),
+	offsetof(struct rtnl_link_stats64, tx_dropped),
+	offsetof(struct rtnl_link_stats64, multicast),
+	offsetof(struct rtnl_link_stats64, collisions),
+	offsetof(struct rtnl_link_stats64, rx_length_errors),
+	offsetof(struct rtnl_link_stats64, rx_over_errors),
+	offsetof(struct rtnl_link_stats64, rx_crc_errors),
+	offsetof(struct rtnl_link_stats64, rx_frame_errors),
+	offsetof(struct rtnl_link_stats64, rx_fifo_errors),
+	offsetof(struct rtnl_link_stats64, rx_missed_errors),
+	offsetof(struct rtnl_link_stats64, tx_aborted_errors),
+	offsetof(struct rtnl_link_stats64, tx_carrier_errors),
+	offsetof(struct rtnl_link_stats64, tx_fifo_errors),
+	offsetof(struct rtnl_link_stats64, tx_heartbeat_errors),
+	offsetof(struct rtnl_link_stats64, tx_window_errors),
+	offsetof(struct rtnl_link_stats64, rx_compressed),
+	offsetof(struct rtnl_link_stats64, tx_compressed),
+	offsetof(struct rtnl_link_stats64, rx_nohandler),
 };
 
-static ssize_t xeth_sysfs_netstat_show(struct kobject *kobj,
+static struct attribute xeth_sysfs_link_stat_attrs[] = {
+	new_xeth_sysfs_attr(rx_packets),
+	new_xeth_sysfs_attr(tx_packets),
+	new_xeth_sysfs_attr(rx_bytes),
+	new_xeth_sysfs_attr(tx_bytes),
+	new_xeth_sysfs_attr(rx_errors),
+	new_xeth_sysfs_attr(tx_errors),
+	new_xeth_sysfs_attr(rx_dropped),
+	new_xeth_sysfs_attr(tx_dropped),
+	new_xeth_sysfs_attr(multicast),
+	new_xeth_sysfs_attr(collisions),
+	new_xeth_sysfs_attr(rx_length_errors),
+	new_xeth_sysfs_attr(rx_over_errors),
+	new_xeth_sysfs_attr(rx_crc_errors),
+	new_xeth_sysfs_attr(rx_frame_errors),
+	new_xeth_sysfs_attr(rx_fifo_errors),
+	new_xeth_sysfs_attr(rx_missed_errors),
+	new_xeth_sysfs_attr(tx_aborted_errors),
+	new_xeth_sysfs_attr(tx_carrier_errors),
+	new_xeth_sysfs_attr(tx_fifo_errors),
+	new_xeth_sysfs_attr(tx_heartbeat_errors),
+	new_xeth_sysfs_attr(tx_window_errors),
+	new_xeth_sysfs_attr(rx_compressed),
+	new_xeth_sysfs_attr(tx_compressed),
+	new_xeth_sysfs_attr(rx_nohandler),
+};
+
+static size_t xeth_sysfs_link_stat_attr_index(struct attribute *attr)
+{
+	return (size_t)(attr-xeth_sysfs_link_stat_attrs);
+}
+
+#define xeth_sysfs_n_link_stats						\
+	sizeof(xeth_sysfs_link_stat_attrs)/sizeof(struct attribute)
+
+static struct attribute
+*xeth_sysfs_link_stat_ktype_default_attrs[1+xeth_sysfs_n_link_stats];
+
+static ssize_t xeth_sysfs_link_stat_show(struct kobject *kobj,
 				       struct attribute *attr,
 				       char *buf)
 {
-	struct xeth_priv *priv = to_xeth_priv(kobj);
-	struct xeth_sysfs_netstat *ns = to_xeth_sysfs_netstat(attr);
+	struct xeth_priv *priv = xeth_priv_of_link(kobj);
+	size_t index = xeth_sysfs_link_stat_attr_index(attr);
+	size_t offset = xeth_sysfs_link_stat_offsets[index];
 	u64 val = 0;
-	mutex_lock(&priv->mutex.stats);
-	val = *(u64 *)(((u8 *) &priv->stats) + ns->offset);
-	mutex_unlock(&priv->mutex.stats);
+	mutex_lock(&priv->link_mutex);
+	val = *(u64 *)(((u8 *) &priv->link_stats) + offset);
+	mutex_unlock(&priv->link_mutex);
 	return scnprintf(buf, PAGE_SIZE, "%llu\n", val);
 }
 
-static ssize_t xeth_sysfs_netstat_store(struct kobject *kobj,
+static ssize_t xeth_sysfs_link_stat_store(struct kobject *kobj,
 					struct attribute *attr,
 					const char *buf, size_t count)
 {
-	struct xeth_priv *priv = to_xeth_priv(kobj);
-	struct xeth_sysfs_netstat *ns = to_xeth_sysfs_netstat(attr);
-	u64 val, *p = (u64 *)(((u8 *) &priv->stats) + ns->offset);
+	struct xeth_priv *priv = xeth_priv_of_link(kobj);
+	size_t index = xeth_sysfs_link_stat_attr_index(attr);
+	size_t offset = xeth_sysfs_link_stat_offsets[index];
+	u64 val, *p = (u64 *)(((u8 *) &priv->link_stats) + offset);
 	int err = xeth_debug_true_val("%d", kstrtoull(buf, 10, &val));
 	if (!err) {
-		mutex_lock(&priv->mutex.stats);
+		mutex_lock(&priv->link_mutex);
 		*p = val;
-		mutex_unlock(&priv->mutex.stats);
+		mutex_unlock(&priv->link_mutex);
 	}
 	return count;
 }
 
-static const struct sysfs_ops xeth_sysfs_netstat_ops = {
-	.show   = xeth_sysfs_netstat_show,
-	.store  = xeth_sysfs_netstat_store,
+static const struct sysfs_ops xeth_sysfs_link_stat_ops = {
+	.show   = xeth_sysfs_link_stat_show,
+	.store  = xeth_sysfs_link_stat_store,
 };
 
-static struct kobj_type xeth_sysfs_netstat_ktype = {
+static struct kobj_type xeth_sysfs_link_stat_ktype = {
 	.release        = NULL,
-	.sysfs_ops      = &xeth_sysfs_netstat_ops,
-	.default_attrs  = xeth_sysfs_netstat_attrs,
+	.sysfs_ops      = &xeth_sysfs_link_stat_ops,
+	.default_attrs  = xeth_sysfs_link_stat_ktype_default_attrs,
 };
 
-static ssize_t xeth_sysfs_rcv_write(struct file *f,
-				    struct kobject *kobj,
-				    struct bin_attribute *attr,
-				    char *buf,
-				    loff_t pos,
-				    size_t len)
+static ssize_t xeth_sysfs_ethtool_stat_show(struct kobject *kobj,
+					    struct attribute *attr,
+					    char *buf)
 {
-	struct sk_buff *skb = netdev_alloc_skb(xeth.iflinks[0], len);
-	if (!skb)
-		return -ENOMEM;
-	skb_put(skb, len);
-	memcpy(skb->data, buf, len);
-	return xeth.ops.side_band_rx(skb);
+	struct xeth_priv *priv = xeth_priv_of_ethtool(kobj);
+	size_t index = xeth.ops.ethtool_stat_attr_index(attr);
+	u64 val = 0;
+	mutex_lock(&priv->ethtool_mutex);
+	val = priv->ethtool_stats[index];
+	mutex_unlock(&priv->ethtool_mutex);
+	return scnprintf(buf, PAGE_SIZE, "%llu\n", val);
 }
 
-struct bin_attribute xeth_sysfs_rcv_bin_attr = {
-	.attr = {
-		.name = "receive",
-		.mode = S_IWUSR,
-	},
-	.size = PAGE_SIZE,		/* FIXME MTU */
-	.write = xeth_sysfs_rcv_write,
+static ssize_t xeth_sysfs_ethtool_stat_store(struct kobject *kobj,
+					     struct attribute *attr,
+					     const char *buf, size_t count)
+{
+	struct xeth_priv *priv = xeth_priv_of_ethtool(kobj);
+	size_t index = xeth.ops.ethtool_stat_attr_index(attr);
+	u64 val;
+	int err = xeth_debug_true_val("%d", kstrtoull(buf, 10, &val));
+	if (!err) {
+		mutex_lock(&priv->ethtool_mutex);
+		priv->ethtool_stats[index] = val;
+		mutex_unlock(&priv->ethtool_mutex);
+	}
+	return count;
+}
+
+static const struct sysfs_ops xeth_sysfs_ethtool_stat_ops = {
+	.show   = xeth_sysfs_ethtool_stat_show,
+	.store  = xeth_sysfs_ethtool_stat_store,
 };
 
 void xeth_sysfs_init(const char *name)
 {
+	int i;
+
+	for (i = 0; i < xeth_sysfs_n_link_stats; i++)
+		xeth_sysfs_link_stat_ktype_default_attrs[i] =
+			&xeth_sysfs_link_stat_attrs[i];
+	xeth_sysfs_link_stat_ktype.default_attrs =
+		xeth_sysfs_link_stat_ktype_default_attrs;
+
 	xeth.sysfs.root = root_device_register(name);
 	if (IS_ERR_OR_NULL(xeth.sysfs.root))
 		return;
-	if (1)
-	xeth_debug_true_val("%d",
-			    sysfs_create_bin_file(&xeth.sysfs.root->kobj,
-						  &xeth_sysfs_rcv_bin_attr));
+	if (xeth.n.ethtool_stats) {
+		xeth.ethtool_stat_ktype.release = NULL;
+		xeth.ethtool_stat_ktype.sysfs_ops =
+			&xeth_sysfs_ethtool_stat_ops;
+	}
 }
 
 void xeth_sysfs_exit(void)
@@ -183,17 +205,27 @@ void xeth_sysfs_exit(void)
 
 void xeth_sysfs_priv_init(struct xeth_priv *priv, const char *ifname)
 {
-	priv->kobj_err = -EINVAL;
+	priv->link_kobj_err = -EINVAL;
 	if (IS_ERR_OR_NULL(xeth.sysfs.root))
 		return;
-	priv->kobj_err = kobject_init_and_add(&priv->kobj,
-					      &xeth_sysfs_netstat_ktype,
-					      &xeth.sysfs.root->kobj,
-					      ifname);
+	priv->link_kobj_err =
+		kobject_init_and_add(&priv->link_kobj,
+				     &xeth_sysfs_link_stat_ktype,
+				     &xeth.sysfs.root->kobj,
+				     ifname);
+	if (priv->link_kobj_err)
+		return;
+	if (!xeth.n.ethtool_stats)
+		return;
+	priv->ethtool_kobj_err =
+		kobject_init_and_add(&priv->ethtool_kobj,
+				     &xeth.ethtool_stat_ktype,
+				     &priv->link_kobj,
+				     "ethtool");
 }
 
 void xeth_sysfs_priv_exit(struct xeth_priv *priv)
 {
-	if (!priv->kobj_err)
-		kobject_put(&priv->kobj);
+	if (!priv->link_kobj_err)
+		kobject_put(&priv->link_kobj);
 }
