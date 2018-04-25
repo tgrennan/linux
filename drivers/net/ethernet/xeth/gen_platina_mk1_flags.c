cfbb5a5019f962c10064813865a1b6d63d878496
Tom Grennan <tgrennan@platinasystems.com>
2018-04-25 15:31:26 -0700

    xeth: gen and test ethtool flag bits
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- drivers/net/ethernet/xeth/gen_platina_mk1_flags.c
+++ drivers/net/ethernet/xeth/gen_platina_mk1_flags.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
+#include <ctype.h>
 
 extern const char *const platina_mk1_flags[];
 
 int main(int argc, char **argv)
 {
 	int i;
-	printf("// Created by gen-platina-mk1-flags -- DO NOT EDIT\n");
-	printf("\n");
-	printf("package main\n");
-	printf("\n");
-	printf("var flags = []string{\n");
+	printf("%s",
+	       "// Created by gen-platina-mk1-flags -- DO NOT EDIT\n"
+	       "\n"
+	       "package main\n"
+		"\n"
+		"const (\n"
+	);
+	printf("\t%c%sBit uint = iota\n", toupper(platina_mk1_flags[0][0]),
+	       platina_mk1_flags[0]+1);
+	for (i = 1; platina_mk1_flags[i]; i++)
+		printf("\t%c%sBit\n", toupper(platina_mk1_flags[i][0]),
+		       platina_mk1_flags[i]+1);
+	printf("%s",
+		")\n"
+		"\n"
+		"var flags = []string{\n"
+	);
 	for (i = 0; platina_mk1_flags[i]; i++)
 		printf("\t\"%s\",\n", platina_mk1_flags[i]);
 	printf("}\n");
