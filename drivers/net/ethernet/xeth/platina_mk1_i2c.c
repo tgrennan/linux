f41b0288b559fae03be917aab7d2fb2a98d89bbb
Tom Grennan <tgrennan@platinasystems.com>
2019-01-15 16:44:15 -0800

    xeth: use onie module to retrieve EEPROM data
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- drivers/net/ethernet/xeth/platina_mk1_i2c.c
+++ drivers/net/ethernet/xeth/platina_mk1_i2c.c
@@ -22,88 +22,181 @@
  * Platina Systems, 3180 Del La Cruz Blvd, Santa Clara, CA 95054
  */
 
+#include <linux/delay.h>
 #include <linux/i2c.h>
+#include <linux/onie.h>
 
 #include "platina_mk1.h"
 
-static struct i2c_adapter *platina_mk1_i2c0;
-static const struct i2c_board_info const platina_mk1_i2c_mezzanine_temp_bi = {
-	I2C_BOARD_INFO("lm75", 0X4f),
+enum { platina_mk1_i2c_eeprom_pgsz = 32 };
+
+static struct {
+	struct i2c_adapter *adapter;
+	struct {
+		const struct i2c_board_info info;
+		struct i2c_client *client;
+	} mezzanine_temp;
+	struct {
+		const struct property_entry properties[3];
+		const struct i2c_board_info info;
+		struct i2c_client *client;
+		u8 data[onie_max_data];
+		void *onie;
+	} eeprom;
+} platina_mk1_i2c = {
+	.mezzanine_temp = {
+		.info = {
+			I2C_BOARD_INFO("lm75", 0X4f),
+		},
+	},
+	.eeprom = {
+		.properties = {
+			PROPERTY_ENTRY_U32("pagesize",
+					   platina_mk1_i2c_eeprom_pgsz),
+			PROPERTY_ENTRY_BOOL("no-read-rollover"),
+			{ },
+		},
+		.info = {
+			I2C_BOARD_INFO("24c04", 0X51),
+			.properties = platina_mk1_i2c.eeprom.properties,
+		},
+	},
 };
-static const struct property_entry platina_mk1_i2c_eeprom_properties[] = {
-	PROPERTY_ENTRY_U32("pagesize", 64),
-	PROPERTY_ENTRY_BOOL("no-read-rollover"),
-	{ }
-};
-static const struct i2c_board_info const platina_mk1_i2c_eeprom_bi = {
-	I2C_BOARD_INFO("24c04", 0X51),
-	.properties = platina_mk1_i2c_eeprom_properties,
-};
-static struct i2c_client *platina_mk1_i2c_mezzanine_temp;
-static struct i2c_client *platina_mk1_i2c_eeprom;
+
+static ssize_t platina_mk1_i2c_eeprom_load(void);
+static int platina_mk1_i2c_eeprom_rewind(void);
+static ssize_t platina_mk1_i2c_eeprom_read(size_t offset, size_t count);
+
+#define platina_mk1_i2c_get_adapter(NR)					\
+({									\
+	platina_mk1_i2c.adapter = i2c_get_adapter(NR);			\
+	(platina_mk1_i2c.adapter ? 0 : -ENODEV);			\
+})
+
+#define platina_mk1_i2c_new_device(NAME)				\
+({									\
+	platina_mk1_i2c.NAME.client =					\
+		i2c_new_device(platina_mk1_i2c.adapter,			\
+			       &platina_mk1_i2c.NAME.info);		\
+	(platina_mk1_i2c.NAME.client ? 0 : -ENODEV);			\
+})
 
 int __init platina_mk1_i2c_init(void)
 {
-	int i, err;
-	const size_t eeprom_size = 256;
-	char *eeprom_cache;
+	struct kobject *parent, *onie;
+	ssize_t sz;
+	int err;
+	u8 v[ETH_ALEN];
 
-	{
-		/* FIXME set xeth.ea from eeprom instead of eth0 */
-		struct net_device *eth0 = dev_get_by_name(&init_net, "eth0");
-		if (eth0 == NULL)
-			return -ENOENT;
-		xeth.ea.base = 3 + ether_addr_to_u64(eth0->dev_addr);
-		xeth.ea.assign_type = eth0->addr_assign_type;
-		dev_put(eth0);
+	err = platina_mk1_i2c_get_adapter(0);
+	if (err)
+		goto platina_mk1_i2c_init_err;
+
+	err = platina_mk1_i2c_new_device(mezzanine_temp);
+	if (err)
+		goto platina_mk1_i2c_init_err;
+
+	err = platina_mk1_i2c_new_device(eeprom);
+	if (err)
+		goto platina_mk1_i2c_init_err;
+
+	sz = platina_mk1_i2c_eeprom_load();
+	if (sz < 0) {
+		err = sz;
+		goto platina_mk1_i2c_init_err;
 	}
 
-	platina_mk1_i2c0 = i2c_get_adapter(0);
-	err = xeth_pr_is_err_val(platina_mk1_i2c0);
-	if (err)
-		goto egress;
+	parent = &platina_mk1_i2c.eeprom.client->dev.kobj;
+	onie = onie_create(parent, platina_mk1_i2c.eeprom.data, NULL);
+	if (IS_ERR(platina_mk1_i2c.eeprom.onie)) {
+		err = PTR_ERR(onie);
+		goto platina_mk1_i2c_init_err;
+	}
+	platina_mk1_i2c.eeprom.onie = onie;
 
-	platina_mk1_i2c_mezzanine_temp =
-		i2c_new_device(platina_mk1_i2c0,
-			       &platina_mk1_i2c_mezzanine_temp_bi);
-	err = xeth_pr_is_err_val(platina_mk1_i2c_mezzanine_temp);
-	if (err)
-		goto egress;
+	sz = onie_value(onie, onie_type_mac_base, ETH_ALEN, v);
+	if (sz == ETH_ALEN) {
+		xeth.ea.base = 4 + ether_addr_to_u64(v);
+		xeth.ea.assign_type = NET_ADDR_PERM;
+	} else {
+		eth_random_addr(v);
+		xeth.ea.base = ether_addr_to_u64(v);
+		xeth.ea.assign_type = NET_ADDR_RANDOM;
+	}
 
-	platina_mk1_i2c_eeprom =
-		i2c_new_device(platina_mk1_i2c0, &platina_mk1_i2c_eeprom_bi);
-	err = xeth_pr_is_err_val(platina_mk1_i2c_eeprom);
-	if (err)
-		goto egress;
-
-	eeprom_cache = kzalloc(eeprom_size, GFP_KERNEL);
-	err = xeth_pr_is_err_val(eeprom_cache);
-	if (err)
-		goto egress;
-
-	i2c_smbus_write_byte_data(platina_mk1_i2c_eeprom, 0, 0);
-	msleep(10);
-	for (i = 0; i < eeprom_size; i++)
-		eeprom_cache[i] = i2c_smbus_read_byte(platina_mk1_i2c_eeprom);
-
-	i2c_smbus_write_byte_data(platina_mk1_i2c_eeprom, 0, 0);
-
-	xeth_pr_buf_hex_dump(eeprom_cache, eeprom_size);
-
-	kfree(eeprom_cache);
+	sz = onie_value(onie, onie_type_device_version, sizeof(u8), v);
+	if (sz == sizeof(u8) && v[0] == 0)
+		xeth.base = 0;
 	return 0;
-egress:
+
+platina_mk1_i2c_init_err:
 	platina_mk1_i2c_exit();
 	return err;
 }
 
 void platina_mk1_i2c_exit(void)
 {
-	if (platina_mk1_i2c_mezzanine_temp)
-		i2c_unregister_device(platina_mk1_i2c_mezzanine_temp);
-	if (platina_mk1_i2c_eeprom)
-		i2c_unregister_device(platina_mk1_i2c_eeprom);
-	if (platina_mk1_i2c0)
-		i2c_put_adapter(platina_mk1_i2c0);
+	if (platina_mk1_i2c.eeprom.onie)
+		onie_delete(platina_mk1_i2c.eeprom.onie);
+	platina_mk1_i2c.eeprom.onie = NULL;
+	if (platina_mk1_i2c.eeprom.client)
+		i2c_unregister_device(platina_mk1_i2c.eeprom.client);
+	platina_mk1_i2c.eeprom.client = NULL;
+	if (platina_mk1_i2c.mezzanine_temp.client)
+		i2c_unregister_device(platina_mk1_i2c.mezzanine_temp.client);
+	platina_mk1_i2c.mezzanine_temp.client = NULL;
+	if (platina_mk1_i2c.adapter)
+		i2c_put_adapter(platina_mk1_i2c.adapter);
+	platina_mk1_i2c.adapter = NULL;
 }
 
+static ssize_t platina_mk1_i2c_eeprom_load()
+{
+	u8 *data = platina_mk1_i2c.eeprom.data;
+	int err, tries;
+	ssize_t rem, sz;
+
+	/* we retry b/c the EEPROM driver probe interferes w/ first read */
+	for (tries = 0; tries < 3; tries++) {
+		msleep(10);
+		err = platina_mk1_i2c_eeprom_rewind();
+		if (err)
+			continue;
+		msleep(10);
+		sz = max_t(size_t, platina_mk1_i2c_eeprom_pgsz, onie_min_data);
+		err = platina_mk1_i2c_eeprom_read(0, sz);
+		if (err < 0)
+			return err;
+		rem = onie_validate(data, sz);
+		if (rem < 0)
+			continue;
+		if (rem == 0)
+			break;
+		err = platina_mk1_i2c_eeprom_read(sz, rem);
+		if (err < 0)
+			return err;
+		sz = onie_validate(data, 0);
+		if (sz > 0)
+			return sz;
+	}
+	return 0;
+}
+
+static int platina_mk1_i2c_eeprom_rewind()
+{
+	return i2c_smbus_write_byte_data(platina_mk1_i2c.eeprom.client, 0, 0);
+}
+
+static ssize_t platina_mk1_i2c_eeprom_read(size_t offset, size_t count)
+{
+	int i;
+	s32 ret;
+
+	for (i = 0; i < count; i++) {
+		ret = i2c_smbus_read_byte(platina_mk1_i2c.eeprom.client);
+		if (ret < 0)
+			return ret;
+		platina_mk1_i2c.eeprom.data[offset+i] = ret;
+	}
+	return count;
+}
