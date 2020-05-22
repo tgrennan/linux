aa7c73d52ba1e12884e5df6827ffadb6f5e36ebe
Tom Grennan <tgrennan@platinasystems.com>
2020-05-22 15:05:41 -0700

    xeth: qsfp: scan from get_module_info
    Scan for module from get_module_info if the i2c client is not yet
    available.
    
    Signed-off-by: Tom Grennan <tgrennan@platinasystems.com>

--- drivers/net/ethernet/xeth/xeth_qsfp.c
+++ drivers/net/ethernet/xeth/xeth_qsfp.c
@@ -14,104 +14,66 @@ enum xeth_qsfp_module_id {
 	xeth_qsfp_module_id_qsfp28	= 0x11,
 };
 
-const struct i2c_device_id xeth_qsfp_id_table[] = {
+/* @xeth_qsfp_bus: a -1 terminated list */
+const int *xeth_qsfp_bus;
+
+static const struct i2c_device_id xeth_qsfp_id_table[] = {
 	{ "qsfp", 0 },
 	{ }
 };
 MODULE_DEVICE_TABLE(i2c, xeth_qsfp_id_table);
 
-const u32 *xeth_qsfp_xids;
-size_t xeth_qsfp_n_xids;
+enum {
+	xeth_qsfp_i2c_class = I2C_CLASS_HWMON | I2C_CLASS_DDC | I2C_CLASS_SPD,
+};
+
+static int xeth_qsfp_detect(struct i2c_client *, struct i2c_board_info *);
+static int xeth_qsfp_probe(struct i2c_client *);
+static int xeth_qsfp_remove(struct i2c_client *);
+static int xeth_qsfp_load(struct i2c_client *, u8 *, u32, u32);
+static int xeth_qsfp_bload(struct i2c_client *qsfp, u8 *data, u32 o, u32 n);
+static int xeth_qsfp_xload(struct i2c_client *qsfp, u8 *data, u32 o, u32 n);
+static struct i2c_client *xeth_qsfp_scan(struct net_device *);
+static int xeth_qsfp_peek(struct i2c_adapter *, unsigned short);
+
+static struct i2c_driver xeth_qsfp_driver = {
+	.class		= xeth_qsfp_i2c_class,
+	.driver = {
+		.name	= "xeth-platina-mk1-qsfp",
+	},
+	.probe_new	= xeth_qsfp_probe,
+	.remove		= xeth_qsfp_remove,
+	.id_table	= xeth_qsfp_id_table,
+	.detect		= xeth_qsfp_detect,
+	.address_list	= I2C_ADDRS(0x50, 0x51),
+};
 
 static struct spinlock xeth_qsfp_mutex;
 
-static int xeth_qsfp_load(struct i2c_client *, u8 *, u32, u32);
-static struct net_device *xeth_qsfp_nd(struct i2c_client *);
-
-int xeth_qsfp_detect(struct i2c_client *qsfp, struct i2c_board_info *info)
+int xeth_qsfp_register_driver(void)
 {
-	struct net_device *nd = xeth_qsfp_nd(qsfp);
-	u8 b[2];
-	u32 n = ARRAY_SIZE(b);
-	int err = 0;
-
-	if (!i2c_check_functionality(qsfp->adapter, I2C_FUNC_SMBUS_BYTE_DATA))
-		return -ENODEV;
-
-	if (!nd)
-		return -ENXIO;
-
-	if (xeth_qsfp_load(qsfp, b, 0, n) < 0)
-		return -ENXIO;
-
-	switch (b[0]) {
-	case xeth_qsfp_module_id_qsfp:
-	case xeth_qsfp_module_id_qsfp_plus:
-	case xeth_qsfp_module_id_qsfp28:
-	case xeth_qsfp_module_id_sfp:
-		strlcpy(info->type, "qsfp", I2C_NAME_SIZE);
-		info->addr = qsfp->addr;
-		xeth_debug_nd(nd, "detected qsfp type 0x%x", b[0]);
-		break;
-	default:
-		xeth_debug_nd(nd, "detected unsupported qsfp type 0x%x", b[0]);
-		err = -ENXIO;
-	}
-	return err;
+	return i2c_add_driver(&xeth_qsfp_driver);
 }
 
-int xeth_qsfp_probe(struct i2c_client *qsfp)
+void xeth_qsfp_unregister_driver(void)
 {
-	struct net_device *nd = xeth_qsfp_nd(qsfp);
-	u8 b[2];
-	u32 n = ARRAY_SIZE(b);
-	int err = 0;
-
-	if (!nd)
-		return -ENODEV;
-
-	if (xeth_qsfp_load(qsfp, b, 0, n) < 0)
-		return -ENXIO;
-
-	switch (b[0]) {
-	case xeth_qsfp_module_id_qsfp:
-	case xeth_qsfp_module_id_qsfp_plus:
-	case xeth_qsfp_module_id_qsfp28:
-	case xeth_qsfp_module_id_sfp:
-		xeth_upper_set_qsfp(nd, qsfp);
-		xeth_debug_nd(nd, "probed qsfp type 0x%x", b[0]);
-		break;
-	default:
-		xeth_debug_nd(nd, "probed unsupported qsfp type 0x%x", b[0]);
-		err = -ENXIO;
-	}
-	return err;
-}
-
-int xeth_qsfp_remove(struct i2c_client *qsfp)
-{
-	struct net_device *nd = xeth_qsfp_nd(qsfp);
-
-	if (!nd)
-		return -ENODEV;
-
-	xeth_upper_set_qsfp(nd, NULL);
-
-	xeth_debug_nd(nd, "qsfp removed from bus %d, addr 0x%x",
-		      qsfp->adapter->nr, qsfp->addr);
-	return 0;
+	i2c_del_driver(&xeth_qsfp_driver);
 }
 
 int xeth_qsfp_get_module_info(struct net_device *nd,
 			      struct ethtool_modinfo *emi)
 {
-	struct i2c_client *qsfp = xeth_upper_qsfp(nd);
+	struct i2c_client *qsfp;
 	u8 b[2];
 	u32 n = ARRAY_SIZE(b);
 	int err;
 
-	if (!qsfp)
-		return -ENODEV;
+	qsfp = xeth_upper_qsfp(nd);
+	if (!qsfp) {
+		qsfp = xeth_qsfp_scan(nd);
+		if (!qsfp)
+			return -ENODEV;
+	}
 	/* Read first 2 bytes to get Module & REV ID */
 	err = xeth_qsfp_load(qsfp, b, 0, n);
 	if (err < 0)
@@ -163,8 +125,54 @@ int xeth_qsfp_get_module_eeprom(struct net_device *nd,
 	return err < 0 ? err : 0;
 }
 
-static int xeth_qsfp_bload(struct i2c_client *qsfp, u8 *data, u32 o, u32 n);
-static int xeth_qsfp_xload(struct i2c_client *qsfp, u8 *data, u32 o, u32 n);
+static int xeth_qsfp_detect(struct i2c_client *qsfp,
+			    struct i2c_board_info *info)
+{
+	int i;
+
+	if (!xeth_qsfp_bus)
+		return -ENXIO;
+
+	for (i = 0; xeth_qsfp_bus[i] != qsfp->adapter->nr; i++)
+		if (xeth_qsfp_bus[i] < 0)
+			return -ENXIO;
+
+	if (!xeth_qsfp_peek(qsfp->adapter, qsfp->addr))
+		return -ENXIO;
+
+	strscpy(info->type, "qsfp", sizeof(info->type));
+	xeth_debug("detected qsfp %d, 0x%x", qsfp->adapter->nr, qsfp->addr);
+	return 0;
+}
+
+static int xeth_qsfp_probe(struct i2c_client *qsfp)
+{
+	struct net_device *nd;
+
+	nd = xeth_upper_with_qsfp_bus(qsfp->adapter->nr);
+	if (!nd)
+		return -ENODEV;
+
+	if (!xeth_qsfp_peek(qsfp->adapter, qsfp->addr))
+		return -ENXIO;
+
+	xeth_upper_set_qsfp(nd, qsfp);
+	xeth_debug_nd(nd, "probed qsfp %d, 0x%x", qsfp->adapter->nr, qsfp->addr);
+	return 0;
+}
+
+static int xeth_qsfp_remove(struct i2c_client *qsfp)
+{
+	struct net_device *nd;
+
+	nd = xeth_upper_with_qsfp_bus(qsfp->adapter->nr);
+	if (!nd)
+		return -ENODEV;
+
+	xeth_upper_set_qsfp(nd, NULL);
+	xeth_debug_nd(nd, "removed qsfp %d, 0x%x", qsfp->adapter->nr, qsfp->addr);
+	return 0;
+}
 
 static int xeth_qsfp_load(struct i2c_client *qsfp, u8 *data, u32 o, u32 n)
 {
@@ -215,20 +223,50 @@ static int xeth_qsfp_xload(struct i2c_client *qsfp, u8 *data, u32 o, u32 n)
 	return err < 0 ? err : err == nmsgs ? n : 0;
 }
 
-static struct net_device *xeth_qsfp_nd(struct i2c_client *qsfp)
+static struct i2c_client *xeth_qsfp_scan(struct net_device *nd)
 {
-	struct i2c_adapter *a = qsfp->adapter;
-	struct net_device *nd;
-	u32 xid;
+	struct i2c_client *qsfp = NULL;
+	struct i2c_adapter *adapter;
+	struct i2c_board_info info;
+	int i, nr;
 
-	if (a->nr >= xeth_qsfp_n_xids)
-		return NULL;
+	nr = xeth_upper_qsfp_bus(nd);
+	adapter = i2c_get_adapter(nr);
+	memset(&info, 0, sizeof(info));
+	strscpy(info.type, "qsfp", sizeof(info.type));
+	for (i = 0; xeth_qsfp_driver.address_list[i] != I2C_CLIENT_END; i++) {
+		info.addr = xeth_qsfp_driver.address_list[i];
+		if (xeth_qsfp_peek(adapter, info.addr)) {
+			qsfp = i2c_new_device(adapter, &info);
+			break;
+		}
+	}
+	i2c_put_adapter(adapter);
+	xeth_debug_nd(nd, "scan bus %d %s", nr, qsfp ? "ok" : "failed");
+	return qsfp;
+}
 
-	xid = xeth_qsfp_xids[a->nr];
+/* Returns 1 with matching supported module ID and 0 otherwise. */
+static int xeth_qsfp_peek(struct i2c_adapter *adapter, unsigned short addr)
+{
+	int err;
+	union i2c_smbus_data data;
 
-	rcu_read_lock();
-	nd = xeth_upper_lookup_rcu(xid);
-	rcu_read_unlock();
-
-	return nd;
+	if (!i2c_check_functionality(adapter, I2C_FUNC_SMBUS_READ_BYTE_DATA))
+		return 0;
+	err = i2c_smbus_xfer(adapter, addr, 0, I2C_SMBUS_READ, 0,
+			     I2C_SMBUS_BYTE_DATA, &data);
+	if (err < 0)
+		return 0;
+	switch (data.byte) {
+	case xeth_qsfp_module_id_qsfp:
+	case xeth_qsfp_module_id_qsfp_plus:
+	case xeth_qsfp_module_id_qsfp28:
+	case xeth_qsfp_module_id_sfp:
+		err = 1;
+		break;
+	default:
+		err = 0;
+	}
+	return err;
 }
